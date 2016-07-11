30b7cba37aff6395abc9d8bf6ee9dd8c3c75fc7c
liuhongjian <liuhongjian01@baidu.com>
2016-07-12 07:17:31 +0800

    test reset

--- Demo.java
+++ Demo.java
@@ -2,5 +2,9 @@ class Demo{
     public static void main(String args[]){
       System.out.println("Hello World");
     }
+    
+    public static void test() {
+      // ----------------
+    }
 
 }
